28b179ea4f0def304ea62238eaf3160a65af5a76
Matvey Malkov <work.malkov.matvey@gmail.com>
2024-02-17 22:50:13 +0500

    add terraform workflow

--- terraform/network.tf
+++ terraform/network.tf
@@ -22,20 +22,3 @@ resource "yandex_vpc_subnet" "subnet-c" {
   network_id     = yandex_vpc_network.diploma.id
   v4_cidr_blocks = ["192.168.3.0/24"]
 }
-
-
-# resource "yandex_dns_zone" "malkops" {
-#   name        = "malkops.ru"
-#   description = "malkops public zone"
-
-#   zone             = "malkops.ru."
-#   public           = true
-# }
-
-# resource "yandex_dns_recordset" "rs1" {
-#   zone_id = yandex_dns_zone.malkops.id
-#   name    = "malkops.ru."
-#   type    = "A"
-#   ttl     = 200
-#   data    = ["10.1.0.1"]
-# }
